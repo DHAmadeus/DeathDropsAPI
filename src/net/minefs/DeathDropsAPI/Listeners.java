0aff03027e86fd34034a4e5086f51670eb82c46a
MClausz <themasterclaus@gmail.com>
2016-03-14 16:41:57 +0700

    Added files via upload

---
+++ src/net/minefs/DeathDropsAPI/Listeners.java
@@ -0,0 +1,59 @@
+package net.minefs.DeathDropsAPI;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+public final class Listeners implements Listener {
+
+	@EventHandler
+	public void onDie(PlayerDeathEvent e) {
+		if (e.getKeepInventory())
+			return;
+		Player p = e.getEntity();
+		if (p.hasPermission("deathdropsapi.keep")) {
+			e.setKeepInventory(true);
+			e.setKeepLevel(true);
+			return;
+		}
+		ItemStack[] armors = p.getInventory().getArmorContents();
+		for (int a = 0; a < armors.length; a++) {
+			ItemStack i = armors[a];
+			if(i==null || i.getType().equals(Material.AIR))
+				continue;
+			PlayerDeathDropEvent ev = new PlayerDeathDropEvent(p, i);
+			Bukkit.getPluginManager().callEvent(ev);
+			ItemStack i2 = ev.getItem();
+			if (ev.isCancelled())
+				armors[a] = i2;
+			else {
+				armors[a] = null;
+				p.getWorld().dropItemNaturally(p.getLocation(), i2);
+			}
+		}
+
+		ItemStack[] inventory = p.getInventory().getContents();
+		for (int a = 0; a < inventory.length; a++) {
+			ItemStack i = inventory[a];
+			if(i==null || i.getType().equals(Material.AIR))
+				continue;
+			PlayerDeathDropEvent ev = new PlayerDeathDropEvent(p, i);
+			Bukkit.getPluginManager().callEvent(ev);
+			ItemStack i2 = ev.getItem();
+			if (ev.isCancelled())
+				inventory[a] = i2;
+			else {
+				inventory[a] = null;
+				p.getWorld().dropItemNaturally(p.getLocation(), i2);
+			}
+		}
+
+		p.getInventory().setArmorContents(armors);
+		p.getInventory().setContents(inventory);
+		e.setKeepInventory(true);
+	}
+}
---
+++ src/net/minefs/DeathDropsAPI/PlayerDeathDropEvent.java
@@ -0,0 +1,52 @@
+package net.minefs.DeathDropsAPI;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerDeathDropEvent extends Event implements Cancellable{
+	private Player p;
+	private ItemStack item;
+	private boolean cancelled;
+	private static final HandlerList handlers = new HandlerList();
+
+	
+	public PlayerDeathDropEvent(Player p, ItemStack item) {
+		this.p = p;
+		this.item = item;
+		this.cancelled = false;
+	}
+
+	public ItemStack getItem() {
+		return this.item;
+	}
+
+	public void setItem(ItemStack item) {
+		this.item = item;
+	}
+
+	public Player getPlayer() {
+		return this.p;
+	}
+	
+	@Override
+	public boolean isCancelled(){
+		return this.cancelled;
+	}
+	
+	@Override
+	public void setCancelled(boolean cancelled){
+		this.cancelled = cancelled;
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
