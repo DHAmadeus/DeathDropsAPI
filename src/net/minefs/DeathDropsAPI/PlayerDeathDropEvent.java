ce98a535a2f1944fa019be684df0fdb2f16b03b0
amadeus <themasterclaus@gmail.com>
2019-05-16 13:11:30 +0700

    update

--- src/net/minefs/DeathDropsAPI/PlayerDeathDropEvent.java
+++ src/net/minefs/DeathDropsAPI/PlayerDeathDropEvent.java
@@ -1,3 +1,69 @@
+<<<<<<< HEAD
+package net.minefs.DeathDropsAPI;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerDeathDropEvent extends Event implements Cancellable{
+	private Player p;
+	private ItemStack item;
+	private boolean cancelled;
+	private boolean armor;
+	private static final HandlerList handlers = new HandlerList();
+
+	public PlayerDeathDropEvent(Player p, ItemStack item) {
+		this.p = p;
+		this.item = item;
+		this.cancelled = false;
+		this.armor = true;
+	}
+	
+	public PlayerDeathDropEvent(Player p, ItemStack item, boolean isArmor) {
+		this.p = p;
+		this.item = item;
+		this.cancelled = false;
+		this.armor = isArmor;
+	}
+
+	public boolean isArmor(){
+		return this.armor;
+	}
+	
+	public ItemStack getItem() {
+		return this.item;
+	}
+
+	public void setItem(ItemStack item) {
+		this.item = item;
+	}
+
+	public Player getPlayer() {
+		return this.p;
+	}
+	
+	@Override
+	public boolean isCancelled(){
+		return this.cancelled;
+	}
+	
+	@Override
+	public void setCancelled(boolean cancelled){
+		this.cancelled = cancelled;
+	}
+	
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+}
+=======
 package net.minefs.DeathDropsAPI;
 
 import org.bukkit.entity.Player;
@@ -63,3 +129,4 @@ public class PlayerDeathDropEvent extends Event implements Cancellable{
 		return handlers;
 	}
 }
+>>>>>>> bfb138f675c8620c99273c4732a6bad0c8b24ef5
