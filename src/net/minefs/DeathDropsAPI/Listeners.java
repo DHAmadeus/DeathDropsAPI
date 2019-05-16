0b806ff634d09dd24a1440957fa6f86965cb01e6
amadeus <themasterclaus@gmail.com>
2019-05-16 13:15:28 +0700

    update

--- src/net/minefs/DeathDropsAPI/Listeners.java
+++ src/net/minefs/DeathDropsAPI/Listeners.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package net.minefs.DeathDropsAPI;
 
 import org.bukkit.Bukkit;
@@ -60,64 +59,3 @@ public final class Listeners implements Listener {
 		e.setKeepInventory(true);
 	}
 }
-=======
-package net.minefs.DeathDropsAPI;
-
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.inventory.ItemStack;
-
-public final class Listeners implements Listener {
-
-	@EventHandler (priority = EventPriority.MONITOR)
-	public void onDie(PlayerDeathEvent e) {
-		if (e.getKeepInventory())
-			return;
-		Player p = e.getEntity();
-		if (p.hasPermission("deathdropsapi.keep")) {
-			e.setKeepInventory(true);
-			e.setKeepLevel(true);
-			return;
-		}
-		ItemStack[] armors = p.getInventory().getArmorContents();
-		for (int a = 0; a < armors.length; a++) {
-			ItemStack i = armors[a];
-			if(i==null || i.getType().equals(Material.AIR))
-				continue;
-			PlayerDeathDropEvent ev = new PlayerDeathDropEvent(p, i, true);
-			Bukkit.getPluginManager().callEvent(ev);
-			ItemStack i2 = ev.getItem();
-			if (ev.isCancelled())
-				armors[a] = i2;
-			else {
-				armors[a] = null;
-				p.getWorld().dropItemNaturally(p.getLocation(), i2);
-			}
-		}
-
-		ItemStack[] inventory = p.getInventory().getContents();
-		for (int a = 0; a < inventory.length; a++) {
-			ItemStack i = inventory[a];
-			if(i==null || i.getType().equals(Material.AIR))
-				continue;
-			PlayerDeathDropEvent ev = new PlayerDeathDropEvent(p, i);
-			Bukkit.getPluginManager().callEvent(ev);
-			ItemStack i2 = ev.getItem();
-			if (ev.isCancelled())
-				inventory[a] = i2;
-			else {
-				inventory[a] = null;
-				p.getWorld().dropItemNaturally(p.getLocation(), i2);
-			}
-		}
-
-		p.getInventory().setArmorContents(armors);
-		p.getInventory().setContents(inventory);
-		e.setKeepInventory(true);
-	}
-}
->>>>>>> bfb138f675c8620c99273c4732a6bad0c8b24ef5
